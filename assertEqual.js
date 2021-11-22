59b2185fb13c088a26c35eb4f8a6d3c60bf13924
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-22 23:37:13 +0000

    added new function- to assert the actual vs expected

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\u2705" + " Assertion Passed: ", actual + " === " + expected);
+  } else console.log("\u274C " + "Assertion Failed: ", actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 0);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual('A', 'a');
