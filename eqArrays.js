c676d208b3e11be566145ddcb7207783ad8e91da
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-25 01:49:10 +0000

    adde new condition if length of arrays not equal

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,10 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = function(arr1, arr2) {
   let output = true;
+  //if the length of both arrays are not same return false
+  if(arr1.length !== arr2.length){
+    return false;
+  }
   //compare each element of both the arrays- iterate.
   for(let i = 0; i < arr1.length; i++){
     if(arr1[i] !== arr2[i]){
