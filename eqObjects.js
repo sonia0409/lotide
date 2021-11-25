143e9b140a74a8e929d11c566161903a82a447e7
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-25 01:50:17 +0000

    eqObjects function- compare the two objects

---
+++ eqObjects.js
@@ -0,0 +1,50 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed ${actual} === ${expected}`);
+  } else console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+};
+const eqArrays = function(arr1, arr2) {
+  let output = true;
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  //compare each element of both the arrays- iterate.
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      output = false;
+    }
+  }
+return output;
+}
+// eqObjects function - compare two objects
+const eqObjects = function (object1, object2) {
+  // two objects will be same if:
+  const key1 = Object.keys(object1);
+  const key2 = Object.keys(object2);
+  let output = false;
+  //same number of keys
+  if (key1.length === key2.length) {
+    //if the value of each key in object1 === the value for the same key in object2
+    for (const key of key1) {
+      //iterate - compare the key value in both the objects
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        console.log(object1[key],object2[key])
+        output = eqArrays(object1[key],object2[key])
+      }else if(object1[key] === object2[key]) {
+        output = true;
+      }
+    } 
+  }
+  return output;
+};
+
+
+// TEST CODE
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+//assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
+//console.log(eqArrays(["2", 3],["2", 3, 4]))
