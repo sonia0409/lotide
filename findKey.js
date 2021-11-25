67108644106208b204f904957f1113136acb1992
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-25 22:22:59 +0000

    findKey function added-return the key if callbackfunction return truthy

---
+++ findKey.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed ${actual} === ${expected}`);
+  } else console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+};
+const findKey = function(obj, callback) {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+// TEST CODE
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+console.log(result);
+
+assertEqual(result,"noma");
