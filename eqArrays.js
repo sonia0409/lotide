b907a61e2d43b6782844c98508fa264bc0cb5697
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-29 19:11:43 +0000

    refractor- removed and added assertion tests to another file eqArrayTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,16 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 Assertion Passed ${actual} === ${expected}`);
-  } else console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
-};
+
 const eqArrays = function(arr1, arr2) {
   let output = true;
   //if the length of both arrays are not same return false
-  if(arr1.length !== arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   }
   //compare each element of both the arrays- iterate.
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       output = false;
     }
   }
-return output;
-}
-
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays(["1", ["2", "3"]], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+  return output;
+};
+module.exports = eqArrays;
