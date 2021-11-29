e02710c58fcedf681d58dfca70a9e33478e1d083
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-29 21:19:53 +0000

    mocha and chai used for strict assertion

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,22 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("return 'Hello' for [\"Hello\", \"Lighthouse\", \"Labs\"]",() => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),"Hello");
+  });
+  it("return 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  
+});
