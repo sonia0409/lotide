bfa2f3bbc20fba5115df2ddf0bafddb94e3fed50
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-29 20:07:19 +0000

    refractor- test code moved to another test file'assertArraysEqualTest'

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,10 @@
 // FUNCTION IMPLEMENTATION
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(actual, expected) {
-  let output = true;
-  if(actual.length !== expected.length){
-    return false;
-  }
-  //compare each element of both the arrays- iterate.
-  for(let i = 0; i < actual.length; i++){
-    if(actual[i] !== expected[i]){
-      output = false;
-    }
-  }
-  if (output) {
+  if (eqArrays(actual, expected)) {
     console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
   } else console.log(`\u274C Assertion Failed: ${actual} === ${expected}`);
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [3, 2, 1]); 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); 
+module.exports = assertArraysEqual;
