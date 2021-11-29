ba8f8fffa33aed853da788493cd30f933c5f7449
Sonia Singh <sonia.singh0409@gmail.com>
2021-11-29 18:52:25 +0000

    refractor- added assertion testes to a separate file tailTest file

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705 Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const tail = function(arr) {
+  if(arr)
+
   return arr.splice(1);
 };
-
-// TEST CODE
-assertEqual(tail([5, 6, 7]).length, 2);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
-assertEqual(tail([]), []);
+// export the tail function by referencing the address.
+module.exports = tail;
